488432695f140272e0da12468eb09434b4791e66
Igor-Barross <joseigor32123@gmail.com>
2025-05-26 20:38:40 -0300

    Nivel Novato

---
+++ nivel_novato/nivel_novato.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    int movimentosTorre;
+    int movimentosBispo;
+    int movimentosRainha;
+
+
+    // Movimentação da torre 
+    printf("Digite quantas casas a direita torre irá movimentar (max: 5 casas): \n");
+    scanf("%d", &movimentosTorre);
+    printf("\n");
+
+    
+    printf("Movimentação da torre:\n");
+    for (int i = 1; i <= movimentosTorre; ++i) {        // laço for para fazer repetição dos movimentos
+        printf("%d - Direita\n", i);                    // exibição dos movimentos
+    }
+    printf("\n");
+
+
+    // Movimentação do bispo 
+    printf("Digite quantas casas na diagonal o bispo irá movimentar (max: 5 casas): \n");
+    scanf("%d", &movimentosBispo);
+    printf("\n");
+
+    int qtdMovimentoBispo = 1;
+
+    printf("Movimentação da bispo:\n");
+
+    while (qtdMovimentoBispo <= movimentosBispo) {      // laço while para fazer repetição dos movimentos
+        printf("%d - Cima, Direita\n",                  // exibição dos movimetnos
+           qtdMovimentoBispo);    
+        ++qtdMovimentoBispo;                            // incremento para não virar um loop infinito
+    }
+    printf("\n");
+
+    
+    // Movimentação da rainha 
+    printf("Digite quantas casas a esquerda a rainha irá movimentar (max: 8 casas): \n");
+    scanf("%d", &movimentosRainha);
+    printf("\n");
+
+    int qtdMovimentosRainha = 1;
+
+    printf("Movimentação da rainha:\n");
+    do {                                                // laço do while para fazer repetição dos movimentos
+        printf("%d - esquerda\n",                       // exibição dos movimetnos
+            qtdMovimentosRainha);
+        ++qtdMovimentosRainha;                          // incremento para não virar um loop infinito
+    } while (qtdMovimentosRainha <= movimentosRainha);  // condição para o laço se repetir
+
+
+
+    return 0;
+}
